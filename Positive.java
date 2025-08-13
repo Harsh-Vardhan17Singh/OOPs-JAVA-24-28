1df4b3587a80b11d4b54e23db7416c20520c9d50
Harsh-Vardhan17Singh <harshatwork171827@gmail.com>
2025-08-13 15:34:19 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Positive {
+    public static void main(String[] args) {
+        int x;
+        Scanner sc = new Scanner(System.in);
+          x = sc.nextInt();
+          if(x>0){
+            System.out.println("NO. is Positive");
+            System.out.println("NO. is Good");
+          }
+
+        sc.close();
+
+    }
+}
