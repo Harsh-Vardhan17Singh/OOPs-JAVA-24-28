6e8fad061c34f9cefd3be15681fa0d21c5f67ffa
Harsh-Vardhan17Singh <harshatwork171827@gmail.com>
2025-09-13 11:33:09 +0530

    this Keyword in java
    learning this keyword  in java using a Demo  code

---
+++ ThisDemo.java
@@ -0,0 +1,19 @@
+public class ThisDemo {
+    int a,b;
+    public void get (int x, int y ){
+        a = x;
+        b = y;
+}
+    public void put(){
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+    }
+}
+
+class Demo{
+    public static void main(String[] args) {
+        ThisDemo obj1 = new ThisDemo();
+        obj1.get(10,20);
+        obj1.put();
+    }
+}
